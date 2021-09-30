eb681a61faaa5cfacf382ff6f1c23d4cfde82238
jstrat1618 <jstrat1.618@gmail.com>
2021-09-29 21:11:24 -0600

    Merge to one file

--- app.R
+++ app.R
@@ -1,6 +1,47 @@
 library(shiny)
 
-shinyServer(function(input, output){
+ui <- shinyUI(fluidPage(
+  #App Title
+  titlePanel('Bayes Binomial Proportion with Beta Prior'),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      #HYPER-PARAMETERS
+      #Alpha
+      sliderInput("alpha","Select alpha for the prior distribution", 
+                  min=0.1, max=100, value=1, step=0.1),
+      #Beta
+      sliderInput("beta", "Select beta for the prior distribution" , 
+                  min=0.1, max=100, value=1, step=0.1),
+      
+      #DATA
+      #Number of Trials
+      sliderInput("nTrials", "Select number of Trials", min=2, max=500, value = 100),
+      #Number of heads
+      uiOutput("HeadsOptions"),
+      
+      #GRAPHICAL PARAMETERS
+      radioButtons("color", "Select the color of the plots", 
+                   choices = c("Black","Green", "Blue", "Orange"), selected = "Black")
+      
+    ),
+    
+    
+    
+    
+    mainPanel(
+      plotOutput("prior"),
+      plotOutput("posterior")
+    )
+    
+  )
+  
+))
+
+
+
+server <- shinyServer(function(input, output){
   
   #PLOT THE PRIOR
   output$prior <- renderPlot({
@@ -46,3 +87,7 @@ shinyServer(function(input, output){
   })
   
 })
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
